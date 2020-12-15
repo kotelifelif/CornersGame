5f7da5f6db99fa8fdcf5b36709e09c1ec0df3432
booer <kel42@mail.ru>
2020-12-15 17:36:13 +0700

    Remove Dijkstra algorithm

--- CornersGame/AI.cpp
+++ CornersGame/AI.cpp
@@ -14,15 +14,13 @@
 #include "Constants.h"
 
 
-AI::AI(const bool is_black) : is_black_(is_black), figure_number_(0), move_() {}
+AI::AI(const bool is_black) : is_black_(is_black), figure_number_(0), kMaxDepth(1) {}
 
 int AI::FindBestMove(Board& board, Player& player, int depth) {
   int score = GetHeuristic(board, player);
-  if (depth == 1) {
+  if (depth == kMaxDepth) {
     return score;
   }
-
-  sf::Vector2i best_move(0, 0);
 
   std::vector<Figure> figures = player.GetFigures();
   int figure_number = -1;
@@ -38,181 +36,35 @@ int AI::FindBestMove(Board& board, Player& player, int depth) {
       sum_distance = CheckMove(
           board, player, figure_cell_coordinates,
           sf::Vector2i(figure_cell_coordinates.x - 1, figure_cell_coordinates.y),
-          depth, sum_distance, i, score, Move::kLeft);
+          depth, sum_distance, i, score);
     }
 
     if (figure_cell_coordinates.x + 1 < constants::kRows) {
       sum_distance = CheckMove(
           board, player, figure_cell_coordinates,
           sf::Vector2i(figure_cell_coordinates.x + 1, figure_cell_coordinates.y),
-          depth, sum_distance, i, score, Move::kRight);
+          depth, sum_distance, i, score);
     }
 
     if (figure_cell_coordinates.y - 1 >= 0) {
       sum_distance = CheckMove(
           board, player, figure_cell_coordinates,
           sf::Vector2i(figure_cell_coordinates.x, figure_cell_coordinates.y - 1),
-          depth, sum_distance, i, score, Move::kDown);
+          depth, sum_distance, i, score);
     }
 
     if (figure_cell_coordinates.y + 1 < constants::kColumns) {
       sum_distance = CheckMove(
           board, player, figure_cell_coordinates,
           sf::Vector2i(figure_cell_coordinates.x, figure_cell_coordinates.y + 1),
-          depth, sum_distance, i, score, Move::kUp);
+          depth, sum_distance, i, score);
     }
   }
 
   return sum_distance;
 }
 
-// Implementation of Dijkstra algorithm
-std::vector<Cell> AI::GetShortestPath(const Board& board,
-                                      const Cell& source_cell,
-                                      const Cell& destination_cell) {
-  std::vector<std::vector<Cell>> cells = board.GetCells();
-  std::vector<std::vector<std::vector<Cell>>> adjacent_cells(cells.size());
-  std::vector<std::vector<std::vector<int>>> cost(cells.size());
-  const int kCost = 1;
-
-  for (int i = 0; i < static_cast<int>(cells.size()); ++i) {
-    adjacent_cells.at(i).resize(cells.at(0).size());
-    cost.at(i).resize(cells.at(0).size());
-    for (int j = 0; j < static_cast<int>(cells[i].size()); ++j) {
-      if ((i - 1) >= 0) {
-        if (!cells[i - 1][j].is_busy) {
-          adjacent_cells[i][j].push_back(cells[i - 1][j]);
-          cost[i][j].push_back(kCost);
-        }
-      }
-
-      if ((j - 1) >= 0) {
-        if (!cells[i][j - 1].is_busy) {
-          adjacent_cells[i][j].push_back(cells[i][j - 1]);
-          cost[i][j].push_back(kCost);
-        }
-      }
-
-      if ((i + 1) < static_cast<int>(cells.size())) {
-        if (!cells[i + 1][j].is_busy) {
-          adjacent_cells[i][j].push_back(cells[i + 1][j]);
-          cost[i][j].push_back(kCost);
-        }
-      }
-
-      if ((j + 1) < static_cast<int>(cells[i].size())) {
-        if (!cells[i][j + 1].is_busy) {
-          adjacent_cells[i][j].push_back(cells[i][j + 1]);
-          cost[i][j].push_back(kCost);
-        }
-      }
-    }
-  }
-
-  int max_path_length = constants::kRows * constants::kColumns;
-  std::vector<std::vector<int>> distances(
-      adjacent_cells.size(),
-      std::vector<int>(adjacent_cells.size(), max_path_length));
-  std::vector<std::vector<Cell>> previous_cells(
-      adjacent_cells.size(), std::vector<Cell>(adjacent_cells.size(), Cell()));
-  // priority by distance (first), second - vertex
-  std::vector<std::pair<int, Cell>> processed;
-
-  sf::Vector2i source_coordinates = board.GetCellCoordinates(source_cell);
-  distances[source_coordinates.x][source_coordinates.y] = 0;
-
-  for (size_t i = 0; i < adjacent_cells.size(); ++i) {
-    for (size_t j = 0; j < adjacent_cells[i].size(); ++j) {
-      processed.push_back(std::make_pair(distances[i][j], board.GetCell(i, j)));
-    }
-  }
-
-  while (!processed.empty()) {
-    std::sort(
-        processed.begin(), processed.end(),
-        [&board](std::pair<int, Cell>& left, std::pair<int, Cell>& right) {
-          return left.first > right.first;
-        });
-
-    auto& current = processed.back();
-    int distance = current.first;
-    Cell vertex = current.second;
-    auto coordinates = board.GetCellCoordinates(vertex);
-    processed.pop_back();
-
-    for (size_t i = 0; i < adjacent_cells[coordinates.x][coordinates.y].size();
-         ++i) {
-      Cell neighbor = adjacent_cells[coordinates.x][coordinates.y][i];
-      int neighbor_cost = cost[coordinates.x][coordinates.y][i];
-
-      auto neighbor_coordinates = board.GetCellCoordinates(neighbor);
-      if (distances[neighbor_coordinates.x][neighbor_coordinates.y] >
-          distances[coordinates.x][coordinates.y] + neighbor_cost) {
-        distances[neighbor_coordinates.x][neighbor_coordinates.y] =
-            distances[coordinates.x][coordinates.y] + neighbor_cost;
-        // Maybe change
-        previous_cells[neighbor_coordinates.x][neighbor_coordinates.y] = vertex;
-
-        // Change priority
-        processed.push_back(std::make_pair(
-            distances[neighbor_coordinates.x][neighbor_coordinates.y],
-            neighbor));
-      }
-    }
-  }
-
-  std::vector<Cell> path;
-  sf::Vector2i destination_coordinates =
-      board.GetCellCoordinates(destination_cell);
-
-  // Change for black and white case
-  // If there are no path to destination cell
-  if (distances[destination_coordinates.x][destination_coordinates.y] ==
-      max_path_length) {
-    for (size_t i = 0; i < constants::kRows; ++i) {
-      for (size_t j = 0; j < constants::kColumns; ++j) {
-        if (distances[i][j] != max_path_length) {
-          destination_coordinates.x = i;
-          destination_coordinates.y = j;
-          break;
-        }
-      }
-    }
-  }
-
-  Cell previous_cell =
-      board.GetCell(destination_coordinates.x, destination_coordinates.y);
-  while (previous_cell != source_cell) {
-    path.push_back(previous_cell);
-    sf::Vector2i previous_cell_coordinates =
-        board.GetCellCoordinates(previous_cell);
-    previous_cell = previous_cells[previous_cell_coordinates.x]
-                                  [previous_cell_coordinates.y];
-    previous_cell_coordinates = board.GetCellCoordinates(previous_cell);
-  }
-  std::reverse(path.begin(), path.end());
-
-  return path;
-}
-
-std::pair<sf::Vector2i, int> AI::GetMove(Board& board, Player& player) const {
-  std::vector<Figure> figures = player.GetFigures();
-  Cell figure_cell = board.GetCell(figures[figure_number_].GetPosition());
-  sf::Vector2i figure_cell_coordinates = board.GetCellCoordinates(figure_cell);
-  switch (move_) {
-    case (Move::kDown):
-      figure_cell_coordinates.y--;
-      return std::make_pair(figure_cell_coordinates, figure_number_);
-    case (Move::kLeft):
-      figure_cell_coordinates.x--;
-      return std::make_pair(figure_cell_coordinates, figure_number_);
-    case (Move::kRight):
-      figure_cell_coordinates.x++;
-      return std::make_pair(figure_cell_coordinates, figure_number_);
-    case (Move::kUp):
-      figure_cell_coordinates.y++;
-      return std::make_pair(figure_cell_coordinates, figure_number_);
-  }
+std::pair<sf::Vector2i, int> AI::GetMove(const Board& board, Player& player) const {
   return std::make_pair(destination_coordinates_, figure_number_);
 }
 
@@ -248,26 +100,25 @@ int AI::GetHeuristic(Board& board, Player& player) {
 
 int AI::CheckMove(Board& board, Player& player, sf::Vector2i& old_coordinates,
                   const sf::Vector2i& new_coordinates, int depth,
-                  int sum_distance, int i, int score, const Move move) {
+                  int sum_distance, int figure_number, int score) {
   if (!board.GetCell(new_coordinates.x, new_coordinates.y).is_busy) {
     board.SetBusy(old_coordinates.x, old_coordinates.y, false);
 
     board.SetBusy(new_coordinates.x, new_coordinates.y, true);
-    player.SetFigurePosition(board, i, new_coordinates.x, new_coordinates.y);
+    player.SetFigurePosition(board, figure_number, new_coordinates.x, new_coordinates.y);
 
-    // launch minimax algorithm
+    // Launch minimax algorithm
     score = FindBestMove(board, player, depth + 1);
-    // check if new value is better
+    // Check if new value is better
     if (score < sum_distance) {
-      figure_number_ = i;
+      figure_number_ = figure_number;
       sum_distance = score;
-      move_ = move;
-
+      destination_coordinates_ = new_coordinates;
     }
 
     board.SetBusy(old_coordinates.x, old_coordinates.y, true);
     board.SetBusy(new_coordinates.x, new_coordinates.y, false);
-    player.SetFigurePosition(board, i, old_coordinates.x, old_coordinates.y);
+    player.SetFigurePosition(board, figure_number, old_coordinates.x, old_coordinates.y);
   }
   return sum_distance;
 }
--- CornersGame/AI.h
+++ CornersGame/AI.h
@@ -6,32 +6,21 @@
 
 #include <SFML/Graphics.hpp>
 
-enum class Move {
-    kLeft,
-    kRight,
-    kUp,
-    kDown
-};
-
 class AI {
  public:
-  //std::pair<sf::Vector2i, int> FindBestMove(Board& board, Player& player,
-  //                                          int depth);
   AI(const bool is_black);
   int FindBestMove(Board& board, Player& player,
                                             int depth);
-  std::vector<Cell> GetShortestPath(const Board& board, const Cell& source_cell,
-                                    const Cell& destination_cell);
-  std::pair<sf::Vector2i, int> GetMove(Board& board, Player& player) const; 
+  std::pair<sf::Vector2i, int> GetMove(const Board& board, Player& player) const; 
 
  private:
   bool is_black_;
   int figure_number_;
-  Move move_;
   sf::Vector2i destination_coordinates_;
+  const int kMaxDepth;
   int GetHeuristic(Board& board, Player& player);
   int CheckMove(Board& board, Player& player, sf::Vector2i& old_coordinates,
                  const sf::Vector2i& new_coordinates, int depth, int sum_distance,
-                 int i, int score, const Move move);
+                 int figure_number, int score);
 };
 
